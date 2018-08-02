139d156f08deef2fec77f3c7b217581a577f8548
Jonas Häggqvist <rasher@rasher.dk>
2018-08-02 23:55:11 +0200

    NFL: Add scoring summary to context

--- gamethreads/plugins/nfl/__init__.py
+++ gamethreads/plugins/nfl/__init__.py
@@ -2,6 +2,7 @@ import pytz
 from . import const
 from . import threads
 from . import gamefinder
+from . import nflteams
 from gamethreads.util import NotReadyException
 
 def make_context(game, config):
@@ -13,6 +14,15 @@ def make_context(game, config):
     nfl_game.local_tz = tz
     for event in game.nfl_events:
         event.local_tz = tz
+    # Fix teams in scoring summary
+    scoring = []
+    if game.nfl_data and 'scrsummary' in game.nfl_data.content:
+        scoring = {int(drive_id): drive for drive_id, drive in game.nfl_data.content['scrsummary'].items()}
+        for drive_id in scoring:
+            team_id = scoring[drive_id]['team']
+            team = nflteams.get_team(team_id)
+            scoring[drive_id]['team'] = team
+
     return {
             'game': nfl_game,
             'kickoff': nfl_game.kickoff_utc.astimezone(tz),
@@ -22,4 +32,5 @@ def make_context(game, config):
             'const': {s: getattr(const, s) for s in dir(const) if not s.startswith('_')},
             'lines': {line.book: line for line in game.nfl_lines},
             'forecast': game.nfl_forecast,
+            'scoring': scoring,
             }
