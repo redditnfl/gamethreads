15ead9569dba392126e93550f778268c443761b7
Jonas Häggqvist <rasher@rasher.dk>
2020-09-10 19:01:57 +0200

    NFL: Gamefinder uses nflapi

--- gamethreads/plugins/nfl/gamefinder.py
+++ gamethreads/plugins/nfl/gamefinder.py
@@ -1,13 +1,21 @@
 from gamethreads.util import now
-from . import schedule
 from .const import *
+from sgqlc.operation import Operation
+
+from nflapi import NFL, shield
 
 def find_games():
     """Find find all games occuring within a reasonable timeframe
     
     Return game ids"""
-    games = schedule.get_schedule(*schedule.get_week(now().date()))
-    return [game.eid for game in games]
+    nfl = NFL('gamethread/finder')
+    now = nfl.schedule.current_week()
+
+    op = Operation(shield.Viewer)
+    games = op.viewer.league.games_by_week(week_season_value=now.current_season['default'], week_season_type=now.current_season_type['default'], week_week_value=now.current_week['default'])
+    games.id()
+    result = nfl.query(op)
+    return [g.id for g in result.viewer.league.games_by_week]
 
 def update_state(game):
     """Update the state of a game if necessary"""
